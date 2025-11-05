c35b7d12bbd3932012610a05546b5e0f7386b32c
Valee-ortega <valeom57@gmail.com>
2025-11-05 08:12:58 -0600

    Add files via upload
    corrección del tablero y posicionamiento del jugador 1 en la primera casilla

--- Serpientes_escaleras.asm
+++ Serpientes_escaleras.asm
@@ -1,67 +1,69 @@
+
 extern printf
-extern sprintf ;convertir numeros a strings
-extern scanf
-extern system ;para comandos de la terminal
+extern system
 
 section .data
-    casillas:
-        times 100 db '.',0
+    casillas: times 100 db '.', 0
 
     clear_cmd db 'clear',0
-    serpiente db 'S',0
-    escalera db 'E',0
-    jugador1 db 'P1',0
-    jugador2 db 'P2',0
-    jugador3 db 'P3',0
-    vacio db '.',0
 
-    num_buf db '00',0
+    serpiente db 'S',0
+    escalera  db 'E',0
+    jugador1  db '1',0
+    jugador2  db '2',0
+    jugador3  db '3',0
+    vacio     db '.',0
+    texto_dado db '00',0   ; para guardar el numero del dado como texto
 
     board_format:
-         db '┌─┬─┬─┬─┬─┬─┬─┬─┬─┬─┐',10,0
-         db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10,0
-         db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10,0
-         db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10,0
-         db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10,0
-         db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10,0
-         db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10,0
-         db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10,0
-         db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10,0
-         db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10,0
-         db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10,0
-         db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10,0
-         db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10,0
-         db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10,0
-         db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10,0
-         db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10,0
-         db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10,0
-         db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10,0
-         db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10,0
-         db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10,0
-         db '└─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘',10,0,0
-                 
-
-
-section .bss
+        db '┌─┬─┬─┬─┬─┬─┬─┬─┬─┬─┐',10
+        db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
+        db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10
+        db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
+        db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10
+        db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
+        db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10
+        db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
+        db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10
+        db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
+        db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10
+        db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
+        db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10
+        db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
+        db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10
+        db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
+        db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10
+        db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
+        db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10
+        db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
+        db '├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤',10
+        db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
+        db '└─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘',10,0
 
 section .text
-global main 
+global main
 
 main:
     push ebp
-    mov ebp,esp
+    mov  ebp, esp
 
-    mov ecx,100
-    mov esi,casillas + 297
+    ;  Colocar al jugador 1 en casilla 1
+    mov byte [casillas + 0], '1'
 
+   
+    mov ecx, 100          ; total de casillas
+    mov esi, casillas
+    add esi, 198          ; ultima casilla = (99 * 2)
 .push_loop:
-    push esi
-    sub esi,3
+    push esi              ; empuja dirección de la celda a printf
+    sub  esi, 2
     loop .push_loop
 
     push board_format
     call printf
-    add esp,404
+    add  esp, 404         ; limpiar la pila (100*4 + 4)
 
-    pop ebp
+
+    xor eax, eax
+    leave
     ret
