b3f6e9dcb12e4dcda0a1ec09d0d159a901413e26
Valee-ortega <valeom57@gmail.com>
2025-11-05 11:27:43 -0600

    Add files via upload
    Intento de implementar el dado, y corrección de la posición inicial del jugador

--- Serpientes_escaleras.asm
+++ Serpientes_escaleras.asm
@@ -1,5 +1,6 @@
 
 extern printf
+extern scanf
 extern system
 
 section .data
@@ -14,6 +15,10 @@ section .data
     jugador3  db '3',0
     vacio     db '.',0
     texto_dado db '00',0   ; para guardar el numero del dado como texto
+
+    msg_enter  db 'Presiona ENTER para lanzar el dado...',10,0
+    msg_dado   db 'Dado: %s',10,0
+    fmt_char   db ' %c',0                    
 
     board_format:
         db '┌─┬─┬─┬─┬─┬─┬─┬─┬─┬─┐',10
@@ -40,6 +45,8 @@ section .data
         db '│%s│%s│%s│%s│%s│%s│%s│%s│%s│%s│',10
         db '└─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘',10,0
 
+section .bss
+    tecla resb 1                  ;para leer ENTER
 section .text
 global main
 
@@ -47,8 +54,8 @@ main:
     push ebp
     mov  ebp, esp
 
-    ;  Colocar al jugador 1 en casilla 1
-    mov byte [casillas + 0], '1'
+    ;  colocar al jugador 1 en casilla 1
+    mov byte [casillas + 180], '1'
 
    
     mov ecx, 100          ; total de casillas
@@ -63,6 +70,37 @@ main:
     call printf
     add  esp, 404         ; limpiar la pila (100*4 + 4)
 
+
+    push dword msg_enter        ;pedir enter
+    call printf
+    add  esp, 4
+    
+
+    lea  eax, [tecla]
+    push eax
+    push dword fmt_char
+    call scanf
+    add  esp, 8
+
+     
+    rdtsc                 
+    mov  edx, 0
+    mov  ebx, 6
+    div  ebx              
+    mov  eax, edx         
+    add  eax, 1           
+
+    ; convertir a texto en 'texto_dado'
+    mov  bl, al
+    add  bl, '0'          ; '1'..'6'
+    mov  [texto_dado], bl
+    mov  byte [texto_dado+1], 0
+
+    ; mostrar resultado del dado
+    push dword texto_dado
+    push dword msg_dado
+    call printf
+    add  esp, 8
 
     xor eax, eax
     leave
